44b3e018589c9b73b09a50222265a2bd28aa8cf3
mlynnlato <maxlynnlato@gmail.com>
2023-03-20 20:02:26 -0400

    Implemented Feature 1

--- src/repositories/movie_repository.py
+++ src/repositories/movie_repository.py
@@ -12,7 +12,7 @@ def get_movie_repository():
         """In memory database which is a simple dict of movies"""
 
         def __init__(self) -> None:
-            self._db: dict[int, Movie] = {}
+            self._db: dict[int, Movie] = {1: Movie(1,"Hunger Games", "Bay", 9), 2:Movie(2,"Drive to Survive", "Formula 1", 5), 5: Movie(5,"La La Land", "Ryan", 7)}
 
         def get_all_movies(self) -> dict[int, Movie]:
             """Simply return all movies from the in-memory database"""
