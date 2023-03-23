74dc84a54486ef6e5e6f9d014a80b5936d2c9d3b
Jacopo-Magnani <114836149+Jacopo-Magnani@users.noreply.github.com>
2023-03-22 22:49:23 -0400

    Update movie_repository.py

--- src/repositories/movie_repository.py
+++ src/repositories/movie_repository.py
@@ -11,7 +11,7 @@ def get_movie_repository():
         """In memory database which is a simple dict of movies"""
 
         def __init__(self) -> None:
-            self._db: dict[int, Movie] = {1: Movie(1, "Movie", "Dir Name", 5), 2: Movie(2,"Movie2", "Dir Name", 5)}
+            self._db: dict[int, Movie] = {}
 
         def get_all_movies(self) -> dict[int, Movie]:
             """Simply return all movies from the in-memory database"""
