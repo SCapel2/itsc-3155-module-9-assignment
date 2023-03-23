e91174ee1d89bda317c228c64de6d57dbe3947ce
Sach Denuwara <sddenuwara@gmail.com>
2023-03-22 20:49:31 -0400

    Moved test code from test_create_movie.py to test_create_movies_page.py and rewrote unit test code in test_create_movie.py

--- tests/unit/test_create_movie.py
+++ tests/unit/test_create_movie.py
@@ -6,79 +6,101 @@ from src.models.movie import Movie
 
 movie_repository = get_movie_repository()
 
-# Case 1: Valid movie name, director name and rating
+# Case 1: Valid movie name, director name and rating should create a movie
 def test_create_valid_movies():
     app.testing = True
     client = app.test_client()
+    movie_repository.clear_db()
     
-    response = client.post('/movies', data={'movie_name': 'Hunger Games', 'director_name': 'Gary Ross', 'rating': 3})
-    assert response.status_code == 302 
-    all_movies = movie_repository.get_all_movies()
-    assert len(all_movies) == 1
-    for movie in all_movies.values():
-        assert movie.title == 'Hunger Games'
-        assert movie.director == 'Gary Ross'
-        assert movie.rating == 3
+    test_movie = movie_repository.create_movie('Hunger Games', 'Gary Ross', 3)
+    assert test_movie.title == 'Hunger Games'
+    assert test_movie.director == 'Gary Ross'
+    assert test_movie.rating == 3
+    assert len(movie_repository.get_all_movies()) == 1
 
     movie_repository.clear_db()
 
-# Case 2: No movie name
+# Case 2: No movie name should create a movie
 def test_create_invalid_name_1():
     app.testing = True
     client = app.test_client()
-
-    response = client.post('/movies', data={'movie_name': None, 'director_name': 'Gary Ross', 'rating': 3})
-    all_movies = movie_repository.get_all_movies()
-
-    assert len(all_movies) == 0
     movie_repository.clear_db()
 
-# Case 3: No director name
+    test_movie = movie_repository.create_movie(None, 'Gary Ross', 3)
+    assert test_movie.title == None
+    assert test_movie.director == 'Gary Ross'
+    assert test_movie.rating == 3
+
+    assert len(movie_repository.get_all_movies()) == 1
+
+    movie_repository.clear_db()
+
+# Case 3: No director name should create a movie
 def test_create_invalid_director_1():
     app.testing = True
     client = app.test_client()
-
-    response = client.post('/movies', data={'movie_name': 'Hunger Games', 'director_name': None, 'rating': 3})
-    all_movies = movie_repository.get_all_movies()
-    assert len(all_movies) == 0
     movie_repository.clear_db()
 
-# Case 4a: No rating
+    test_movie = movie_repository.create_movie('Hunger Games', None, 3)
+    assert test_movie.title == 'Hunger Games'
+    assert test_movie.director == None
+    assert test_movie.rating == 3
+    assert len(movie_repository.get_all_movies()) == 1
+
+    movie_repository.clear_db()
+
+# Case 4a: No rating should create a movie
 def test_create_invalid_rating_1():
     app.testing = True
     client = app.test_client()
-
-    response = client.post('/movies', data={'movie_name': 'Hunger Games', 'director_name': 'Gary Ross', 'rating': None})
-    all_movies = movie_repository.get_all_movies()
-    assert len(all_movies) == 0
     movie_repository.clear_db()
 
-# Case 4b: Non-integer rating
+    test_movie = movie_repository.create_movie('Hunger Games', 'Gary Ross', None)
+    assert test_movie.title == 'Hunger Games'
+    assert test_movie.director == 'Gary Ross'
+    assert test_movie.rating == None
+    assert len(movie_repository.get_all_movies()) == 1
+
+    movie_repository.clear_db()
+
+# Case 4b: Non-integer rating should create a movie
 def test_create_invalid_rating_2():
     app.testing = True
     client = app.test_client()
-
-    response = client.post('/movies', data={'movie_name': 'Hunger Games', 'director_name': 'Gary Ross', 'rating': 'abc'})
-    all_movies = movie_repository.get_all_movies()
-    assert len(all_movies) == 0
     movie_repository.clear_db()
 
-# Case 4c: Rating out of range (>5)
+    test_movie = movie_repository.create_movie('Hunger Games', 'Gary Ross', 'abc')
+    assert test_movie.title == 'Hunger Games'
+    assert test_movie.director == 'Gary Ross'
+    assert test_movie.rating == 'abc'
+    assert len(movie_repository.get_all_movies()) == 1
+
+    movie_repository.clear_db()
+
+# Case 4c: Rating out of range (>5) should create a movie
 def test_create_invalid_rating_3():
     app.testing = True
     client = app.test_client()
-
-    response = client.post('/movies', data={'movie_name': 'Hunger Games', 'director_name': 'Gary Ross', 'rating': 6})
-    all_movies = movie_repository.get_all_movies()
-    assert len(all_movies) == 0
     movie_repository.clear_db()
 
-# Case 4d: Rating out of range (<0)
+    test_movie = movie_repository.create_movie('Hunger Games', 'Gary Ross', 6)
+    assert test_movie.title == 'Hunger Games'
+    assert test_movie.director == 'Gary Ross'
+    assert test_movie.rating == 6
+    assert len(movie_repository.get_all_movies()) == 1
+
+    movie_repository.clear_db()
+
+# Case 4d: Rating out of range (<1)
 def test_create_invalid_rating_4():
     app.testing = True
     client = app.test_client()
+    movie_repository.clear_db()
+
+    test_movie = movie_repository.create_movie('Hunger Games', 'Gary Ross', 0)
+    assert test_movie.title == 'Hunger Games'
+    assert test_movie.director == 'Gary Ross'
+    assert test_movie.rating == 0
+    assert len(movie_repository.get_all_movies()) == 1
     
-    response = client.post('/movies', data={'movie_name': 'Hunger Games', 'director_name': 'Gary Ross', 'rating': 0})
-    all_movies = movie_repository.get_all_movies()
-    assert len(all_movies) == 0
     movie_repository.clear_db()
